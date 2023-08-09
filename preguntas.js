1b0d964eca662f5ef956e9020507e43b65f7f9b1
Noel <noelati@gmail.com>
2023-08-09 03:14:35 -0400

    add files

---
+++ preguntas.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "¿Cuántos libros tiene el Antiguo Testamento?",
+    answer: "39",
+    options: [
+      "49",
+      "39",
+      "66",
+      "27"
+    ]
+  },
+  {
+    numb: 2,
+    question: ". ¿Cuántos libros tiene el Pentateuco?",
+    answer: "5",
+    options: [
+      "6",
+      "7",
+      "5",
+      "4"
+    ]
+  },
+  {
+    numb: 3,
+    question: "¿Cómo se divide los libros del Antiguo testamento?",
+    answer: "Todas las anteriores",
+    options: [
+      "Pentateuco, Libros Históricos",
+      "Profetas Mayores, Profetas menores",
+      "Todas las anteriores",
+      "No contiene Divisiones"
+    ]
+  },
+  {
+    numb: 4,
+    question: "¿Quién escribió el Pentateuco?",
+    answer: "Moises",
+    options: [
+      "Abraham",
+      "Jeremias",
+      "Adán",
+      "Moises"
+    ]
+  },
+  {
+    numb: 5,
+    question: "¿Cuáles dos libros del Antiguo Testamento llevan nombres de mujeres?",
+    answer: "Ester y Rut",
+    options: [
+      "Sara y Rebeca",
+      "Ester y Rut",
+      "Lea y Rut",
+      "Ester y Sara"
+    ]
+  },
+];
